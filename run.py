af123a03550838c23721af852669604c43ec320c
philophilo <phil.kwesiga@gmail.com>
2017-11-02 00:37:19 +0300

    modified run file

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import app
 
+app.secret_key = "kj][p]l/=7<F9>j 9877<F10>q2e"
+app.config['SESSION_TYPE'] = "filesystem"
+app.debug=True
 
 if __name__ == "__main__":
-    app.secret_key = "kj][p]l/=7<F9>j 9877<F10>q2e"
-    app.config['SESSION_TYPE'] = "filesystem"
-    app.run(port=3030, debug=True)
+    app.run(port=3030)
