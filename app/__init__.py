a0a87309981cb97b9932347721d8a65fdfbc1b82
philophilo <phil.kwesiga@gmail.com>
2017-11-02 00:40:52 +0300

    modified run file

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,7 @@ app = Flask(__name__, instance_relative_config=True)
 from app import views
 
 app.config.from_object('config.BaseConfig')
+app.secret_key = "kj][p]l/=7<F9>j 9877<F10>q2e"
+app.config['SESSION_TYPE'] = "filesystem"
+app.debug=True
+
--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import app
 
-app.secret_key = "kj][p]l/=7<F9>j 9877<F10>q2e"
-app.config['SESSION_TYPE'] = "filesystem"
-app.debug=True
 
 if __name__ == "__main__":
     app.run(port=3030)
